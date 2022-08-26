52c6024b6cc93483974fd17c78a2d5f1c1a779ed
awekened-VVLD <gritsenkofar2@gmail.com>
2022-08-26 16:20:14 +0300

    Add project files.

---
+++ BlazorChat/Server/SignalRHub.cs
@@ -0,0 +1,31 @@
+﻿using BlazorChat.Shared;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorChat.Server
+{
+    public class SignalRHub : Hub
+    {
+        public async Task SendMessageAsync(ChatMessage message, string userName)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message, userName);
+        }
+        public async Task ChatNotificationAsync(string message, string receiverUserId, string senderUserId)
+        {
+            await Clients.All.SendAsync("ReceiveChatNotification", message, receiverUserId, senderUserId);
+        }
+
+        public async Task DeleteMessage(ChatMessage message) 
+        {
+            await Clients.All.SendAsync("DeleteMessage", message);
+        }
+
+        public async Task EditMessage(ChatMessage message)
+        {
+            await Clients.All.SendAsync("EditMessage", message);
+        }
+    }
+}
